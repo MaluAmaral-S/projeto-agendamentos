1180e05041f73e66dd9eeec9a84ec86b5239a216
Maria Luiza Amaral Santos <maluamaral713@gmail.com>
2025-09-01 16:53:41 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,12 @@
+// src/config/database.js
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: process.env.DB_PORT,
+  logging: false // Desative para não poluir o console
+});
+
+module.exports = sequelize;
